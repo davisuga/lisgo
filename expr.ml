7e3d4f7cfedbed27037d38177c15eebc1974ed5c
Davi Suga <daviciencia1@gmail.com>
2022-05-08 23:34:38 -0300

    created expression module

---
+++ expr.ml
@@ -0,0 +1,14 @@
+open Typ
+
+type expr =
+  | Int of int
+  | String of string
+  | Variable of string
+  | True
+  | Float of float
+  | False
+  | Abstraction of { param_type : typ; param : string; body : expr }
+  | Application of { func : expr; arg : expr }
+  | List of expr list
+  | Unit
+[@@deriving show { with_path = false }]
