fb1f45e9c40c5eb2fec30f51832f748625ff0230
Davi Suga <daviciencia1@gmail.com>
2022-05-21 18:43:47 -0300

    added more ast parsing cases

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,23 @@
+let f = Printf.sprintf
+
+let if_code = {|(if (<= n 1) 
+  (n)
+  (+ (fib (- n 1)) (fib (- n 2))))|}
+
+let sum_fn_code = "(fn n:int (+ n n))"
+let sum_app_code = f "(%s 5)" sum_fn_code
+
 let _ =
-  [ "1"; "\"1\""; "+"; "1.1"; "true"; "false"; "(+ 1)" ]
+  [ "1";
+    "\"1\"";
+    "+";
+    "1.1";
+    "true";
+    "false";
+    "(+ 1)";
+    if_code;
+    sum_app_code;
+    sum_fn_code;
+    f "(def (sum) (fun n:int %s)))" sum_fn_code
+  ]
   |> List.map Lisgo.Lib.print_prog_info
