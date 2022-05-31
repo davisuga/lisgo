4b2b85a4a92210197bb3bd98acf34bcc5be749af
Davi Suga <daviciencia1@gmail.com>
2022-05-31 19:13:00 -0300

    added menhir flags for debugging and inspection

--- bin/main.ml
+++ bin/main.ml
@@ -6,6 +6,7 @@ let if_code = {|(if (<= n 1)
 
 let sum_fn_code = "(fn n:int (+ n n))"
 let sum_app_code = f "(%s 5)" sum_fn_code
+let fib_def = "(def fib (fn n:int (if (<= n 1) n (+ (fib (- n 1)) (fib (- n 2))))))"
 
 let _ =
   [ "1";
@@ -18,6 +19,7 @@ let _ =
     if_code;
     sum_app_code;
     sum_fn_code;
-    f "(def (sum) (fun n:int %s)))" sum_fn_code
+    f "(def (sum) %s)" sum_fn_code;
+    fib_def
   ]
   |> List.map Lisgo.Lib.print_prog_info
