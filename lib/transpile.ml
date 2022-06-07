bf473f70a085a38e18aec99a98baf08f43a06411
Davi Suga <daviciencia1@gmail.com>
2022-06-07 12:21:00 -0300

    added type transpiler

--- lib/transpile.ml
+++ lib/transpile.ml
@@ -15,4 +15,21 @@ module GoAST = struct
     (* | SExpr _mySExpr -> Golang.AST.String "" *)
     | Unit -> Golang.AST.String ""
   (* let of_application ({ func; arg } : Expr.application_expr) =  *)
+  let rec of_typ my_typ =
+    let open Golang.Types in
+    match my_typ with
+    | Typ.TBool -> Golang.Types.TBool
+    | Typ.TInt -> Golang.Types.TInt
+    | Typ.TStr -> Golang.Types.TStr
+    | Typ.TUnit -> Golang.Types.TVoid
+    | Typ.TFloat -> Golang.Types.TFloat32
+    | Typ.TArrow arrow_typ ->
+      Golang.Types.TFunction
+        { args =
+            Typ.unwrap_type_params arrow_typ.param_type
+            |> List.mapi (fun index typ ->
+                   { typ = of_typ typ; name = "arg" ^ string_of_int index });
+          ret = of_typ arrow_typ.body_typ
+        }
+    | Typ.TList list_typ -> Golang.Types.TList (of_typ list_typ)
 end
